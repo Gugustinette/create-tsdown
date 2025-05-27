d8bc376a35ddf0f5bcea9d72e343f80b292abcb6
Gugustinette <mercier.augustin@outlook.fr>
2025-05-27 20:29:22 +0200

    chore: fix vscode, vitest, pnpm and eslint config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  test: {
+    include: ['tests/**/*.test.ts'],
+  },
   server: {
     watch: {
       ignored: ['**/temp/**'],
